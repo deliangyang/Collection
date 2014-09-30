1919e13e9419714db069e34016d9d81d75e7f7dd
hadesf <yang623601391@sina.com>
2014-09-30 13:39:41 +0800

    sort head file

---
+++ sort.h
@@ -0,0 +1,18 @@
+
+#ifndef __SORT_H__
+#define __SORT_H__
+
+void swap(int *a, int *b);
+void bubble_sort(int *arr, int n);
+void quick_sort(int *arr, int start, int end);
+void shell_sort(int *arr, int n);
+void insert_sort(int *arr, int n);
+void select_sort(int *arr, int n);
+void merge(int *arr);
+void merge_sort(int *arr, int n);
+void print_array(int *arr, int n);
+
+
+
+#endif
+
